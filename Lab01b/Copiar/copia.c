93d206bfbfc906e2351d017d5c7793e37e15cfe2
Hyperionlll <mayaramh@hotmail.com>
2022-08-25 01:00:40 -0300

    Finaliza exercicio do lab01b criando programa de copiar arquivos de texto

---
+++ Lab01b/Copiar/copia.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(int argc, char *argv[]) 
+{
+  if (argc != 3) 
+  {
+    printf("Uso indevido do programa. Passar os três parâmetros (programa, "
+           "fonte, destino) .\n");
+    exit(1);
+  }
+  char *caminho_inicial = argv[1];
+  char *caminho_destino = argv[2];
+
+  FILE *arquivo_inicial = fopen(caminho_inicial, "r");
+  FILE *arquivo_final = fopen(caminho_destino, "w+");
+
+  char caractere = fgetc(arquivo_inicial);
+  while (caractere != EOF) 
+  {
+    fputc(caractere, arquivo_final);
+    caractere = fgetc(arquivo_inicial);
+  }
+  fclose(arquivo_inicial);
+  fclose(arquivo_final);
+}
