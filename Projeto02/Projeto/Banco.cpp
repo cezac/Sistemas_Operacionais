2743960a4bcca77cd8b407d733de8201a8e2d4a7
Caio Cezar Oliveira Filardi do Carmo <37853343+cezac@users.noreply.github.com>
2022-11-15 21:21:44 -0300

    Update Banco.cpp

--- Projeto02/Projeto/Banco.cpp
+++ Projeto02/Projeto/Banco.cpp
@@ -13,28 +13,35 @@
 
 
 // Funcao para trasnferir o dinheiro de uma conta a outra
-int transferencia(int quant){
+int transferencia(int quant, int quantTransfer){
     conta clienteA; // Criando a contaA
     conta clienteB; // Criando a contaB
 
-    clienteA.saldo = 100; // Definindo saldo padrao
-    clienteB.saldo = 100; // Definindo saldo padrao
+    clienteA.saldo = 1000; // Definindo saldo padrao
+    clienteB.saldo = 0; // Definindo saldo padrao
+    clienteA.tentativas = 0; // Definindo o n° de tentativas padrao
+    clienteB.tentativas = 0; // Definindo o n° de tentativas padrao
 
-    int valor = quant; // Valor recebe quant
+    for (int i =0; i<quantTransfer; i++){
 
-    if(clienteA.saldo >=valor ){ // Se o saldo do clienteA for igual ou maior ao valor da transferencia
+        if(clienteA.saldo >=quant){ // Se o saldo do clienteA for igual ou maior ao valor da transferencia
 
-        std::cout << std::endl << "Transferencia Concluida com Sucesso" << std::endl;
-        
-        clienteA.saldo -= valor;
-        std::cout << "Saldo da conta A = " << clienteA.saldo << std::endl;
+            std::cout << std::endl << "Transferencia Concluida com Sucesso" << std::endl;
 
-        clienteB.saldo +=valor;
-        std::cout << "Saldo da conta B = " << clienteB.saldo << std::endl;
-    }
-    else{ // Se o saldo do clienteA for infeiror ao valor
-        std::cout << std::endl << " ERRO DE TRANSFENCIA: ";
-        std::cout << std::endl << " Falta de Saldo " << std::endl;
+            clienteA.saldo -= quant;
+            clienteA.tentativas +=1;
+            std::cout << "Saldo da conta A = " << clienteA.saldo << std::endl;
+            std::cout << "Transferencias Executadas na conta A = " << clienteA.tentativas << std::endl << std::endl;
+
+            clienteB.saldo +=quant;
+            clienteB.tentativas +=1;
+            std::cout << "Saldo da conta B = " << clienteB.saldo << std::endl;
+            std::cout << "Transferencias Executadas na conta B = " << clienteB.tentativas << std::endl << std::endl;
+        }
+        else { // Se o saldo do clienteA for infeiror ao valor
+            std::cout << std::endl << " ERRO DE TRANSFENCIA: ";
+            std::cout << std::endl << " Falta de Saldo " << std::endl;
+        }
     }
 
     return 0;
@@ -58,7 +65,7 @@ int main() {
         exit(1);
     }
 
-    transferencia(valorTransacao);
+    transferencia(valorTransacao, 3);
 
     return 0;
 
