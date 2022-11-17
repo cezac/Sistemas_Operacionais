c12e75e4d90813cd17e982f11c2aa93ab1a20ea6
Caio Cezar Oliveira Filardi do Carmo <37853343+cezac@users.noreply.github.com>
2022-11-17 00:50:06 -0300

    Update Banco.h

--- Projeto02/Projeto/Banco.h
+++ Projeto02/Projeto/Banco.h
@@ -5,21 +5,9 @@
 #ifndef PROJETO_BANCO_H
 #define PROJETO_BANCO_H
 
-#include "Banco.h"
-#include "stdio.h"
-#include "stdlib.h"
-#include "malloc.h"
-#include "sys/types.h"
-#include "signal.h"
-#include "sched.h"
-#include <iostream>
-
-
 struct Banco {
      int saldo;
      int tentativas;
 };
 typedef Banco conta;
-
-
 #endif //PROJETO_BANCO_H
